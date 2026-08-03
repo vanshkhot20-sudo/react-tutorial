cf9bd9dd6d2150fb40129df9bde171a7e423ceed
Rahul Khot <rahulkhot@Rahuls-MacBook-Pro.local>
2026-08-03 21:37:51 +0530

    react-example commit

---
+++ Basics/app.js
@@ -0,0 +1,5 @@
+const string = "Welcome Rahul to react js examples";
+export default string; // Default export
+
+// Named export
+export const arr = [10, 20, 30, 40, 50];
---
+++ Basics/script.js
@@ -0,0 +1,6 @@
+import rahul from "./app.js";
+import { arr } from "./app.js";
+let h2 = document.createElement("h2");
+h2.innerText = rahul;
+document.body.appendChild(h2);
+console.log( arr);
